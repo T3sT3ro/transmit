7d75b12c1a87c986fd0a98fac402e29c13c8bbaa
Amadeone <mikolajpolarczyk@interia.pl>
2019-05-03 02:33:37 +0200

    WIP fixes

--- transmit.cpp
+++ transmit.cpp
@@ -10,7 +10,6 @@ int         sockfd;                                     // socket file descripto
 FILE *      outfd = NULL;                               // output file descriptor
 int         fsize;                                      // filesize from argument
 struct sockaddr_in      server_addr;                    // server address struct
-struct in_addr          server_ip;                      // ip to check against
 // suts up the output file and connection
 void setup_environment(struct sockaddr_in &server_address, char **argv) {
     // open output file
@@ -29,12 +28,9 @@ void setup_environment(struct sockaddr_in &server_address, char **argv) {
     bzero(&server_address, sizeof(server_address));
     server_address.sin_family       = AF_INET;
     server_address.sin_port         = htons(strtoul(argv[2], NULL, 10)); // port
-    if (errno) criterr("port conversion failed"); // ERANGE from strtoul;
-    server_address.sin_addr.s_addr  = htonl(INADDR_ANY);
-    if (!inet_pton(AF_INET, argv[1], &server_ip)) // IP
+    if (errno) criterr("port conversion failed");               // ERANGE from strtoul;
+    if (!inet_pton(AF_INET, argv[1], &server_address.sin_addr)) // IP
         criterr("invalid IP address");
-    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-        criterr("bind error");
 }
 
 // requests part of file from server starting at offset with given size
@@ -62,7 +58,7 @@ int main(const int argc, char *argv[]) {
     while (window_size > 0){    // while still exists
         struct sockaddr_in      sender;
         socklen_t               sender_len = sizeof(sender);
-        u_int8_t                buffer[IP_MAXPACKET + 1]; // packet
+        u_int8_t                buffer[IP_MAXPACKET + 1];       // datagram
 
         //request packets
         for (int i = window_it; i < window_size; i++) // request missing packets
@@ -87,7 +83,7 @@ int main(const int argc, char *argv[]) {
         int     r_size;   // received data size
 
         if (sender.sin_port == server_addr.sin_port &&              // check server IP and PORT
-            server_ip.s_addr == sender.sin_addr.s_addr &&
+            server_addr.sin_addr.s_addr == sender.sin_addr.s_addr &&
             sscanf((char*) buffer, "DATA %u %u\n", &r_offset, &r_size) == 2 &&    // check `DATA ...` header
             r_offset/MAX_DATAGRAM_SIZE >= window_it &&              // from current window
             r_offset/MAX_DATAGRAM_SIZE < window_it + window_size &&
